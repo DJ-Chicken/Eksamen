a098cdd785ee93e8b62b6d3407d3bd954458b6de
DJ Chicken <101567066+DJ-Chicken@users.noreply.github.com>
2023-05-25 23:52:10 +0200

    Employees
    Employees tab

--- src/app/components/employees/employees.component.ts
+++ src/app/components/employees/employees.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.scss']
+  styleUrls: ['./employees.component.scss'],
 })
 export class EmployeesComponent {
-
+  employees = [
+    {
+      name: 'John Doe',
+      position: 'CAO',
+      img: '../../../assets/Images/Hologram.png',
+      description: '(Chief Administrator Officer)',
+    },
+    {
+      name: 'John Doe',
+      position: 'CTO',
+      img: '../../../assets/Images/Hologram.png',
+      description: '(Chief Technology Officer)',
+    },
+    {
+      name: 'John Doe',
+      position: 'CLO',
+      img: '../../../assets/Images/Hologram.png',
+      description: '(Chief Learning Officer)',
+    },
+    {
+      name: 'John Doe',
+      position: 'CTO',
+      img: `../../../assets/Images/Hologram.png`,
+      description: '(Chief Technology Officer)',
+    },
+  ];
 }
