00df2afaee5c24efe06982acb0ad6e45b8f03e71
DJ Chicken <101567066+DJ-Chicken@users.noreply.github.com>
2023-05-25 21:33:49 +0200

    Navbar
    Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { FooterComponent } from './components/footer/footer.component';
 import { EmployeesComponent } from './components/employees/employees.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { MapsComponent } from './components/maps/maps.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, FooterComponent, EmployeesComponent, AboutUsComponent, MapsComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.scss"],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#2e6079",
+      },
+    },
   },
   plugins: [],
 };
