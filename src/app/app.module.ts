921520870b779dabd925ea0fe0a81fdc63cceaee
DJ Chicken <101567066+DJ-Chicken@users.noreply.github.com>
2023-05-25 11:19:03 +0200

    Made all Components
    Lagd alle komponentene som skal settes sammen til sist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { MapsComponent } from './components/maps/maps.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavbarComponent, FooterComponent, EmployeesComponent, AboutUsComponent, MapsComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
